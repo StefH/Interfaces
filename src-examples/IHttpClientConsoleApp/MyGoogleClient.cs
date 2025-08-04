c6d1e9d9d1c3a410e51f880e98713d6750bd98a2
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-04 07:45:52 +0200

    2

---
+++ src-examples/IHttpClientConsoleApp/MyGoogleClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IHttpClientConsoleApp;
+
+public class MyGoogleClient
+{
+    private readonly HttpClient _httpClient;
+
+    public MyGoogleClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://google.com");
+
+    }
+
+    public async Task GetAsync()
+    {
+        var response = await _httpClient.GetAsync("imghp");
+        if (response.IsSuccessStatusCode)
+        {
+            var json = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(json);
+        }
+        else
+        {
+            Console.WriteLine($"Request failed: {response.StatusCode}");
+        }
+    }
+}
--- src-examples/IHttpClientConsoleApp/Program.cs
+++ src-examples/IHttpClientConsoleApp/Program.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Net.Http;
 using System.Net.Http.Json;
+using Microsoft.Extensions.DependencyInjection;
 
 var httpClient = new HttpClient();
 var httpClientProxy = new HttpClientProxy(httpClient);
@@ -9,3 +11,15 @@ var todo = await httpClientProxy.GetFromJsonAsync<Todo>("https://jsonplaceholder
 var postResult = await httpClientProxy.PostAsJsonAsync("https://jsonplaceholder.typicode.com/todos", new Todo { Id = 123 });
 var patchResult = await httpClientProxy.PatchAsJsonAsync("https://jsonplaceholder.typicode.com/todos/1", new Todo { Id = 400 });
 var putResult = await httpClientProxy.PutAsJsonAsync("https://jsonplaceholder.typicode.com/todos/1", new Todo { Id = 444 });
+
+IServiceCollection services = new ServiceCollection();
+services.AddHttpClient();
+services.AddSingleton<IIHttpClientFactory, HttpClientFactoryService>();
+
+var sp = services.BuildServiceProvider();
+
+IHttpClient myClient = sp
+    .GetRequiredService<IIHttpClientFactory>()
+    .CreateClient("MyClient");
+var todo2 = await myClient.GetFromJsonAsync<Todo>("https://jsonplaceholder.typicode.com/todos/1");
+Console.WriteLine($"Todo: {todo?.Id}");
