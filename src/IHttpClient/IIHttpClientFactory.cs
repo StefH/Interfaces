03b8598234d5077c2e6dd3f3c005c6c36618eeaa
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-08-06 15:58:09 +0200

    Add method CreateClient()

--- src/IHttpClient/IIHttpClientFactory.cs
+++ src/IHttpClient/IIHttpClientFactory.cs
@@ -6,8 +6,7 @@
 public interface IIHttpClientFactory
 {
     /// <summary>
-    /// Creates and configures an <see cref="IHttpClient"/> instance using the configuration that corresponds
-    /// to the logical name specified by <paramref name="name"/>.
+    /// Creates and configures an <see cref="IHttpClient"/> instance using the configuration that corresponds to the logical name specified by <paramref name="name"/>.
     /// </summary>
     /// <param name="name">The logical name of the client to create.</param>
     /// <returns>A new <see cref="IHttpClient"/> instance.</returns>
@@ -21,4 +20,20 @@ public interface IIHttpClientFactory
     /// </para>
     /// </remarks>
     IHttpClient CreateClient(string name);
+
+    /// <summary>
+    /// Creates and configures an <see cref="IHttpClient"/> instance using the configuration that corresponds to the defaul.
+    /// </summary>
+    /// <param name="name">The logical name of the client to create.</param>
+    /// <returns>A new <see cref="IHttpClient"/> instance.</returns>
+    /// <remarks>
+    /// <para>
+    /// Each call to <see cref="CreateClient()"/> is guaranteed to return a new <see cref="IHttpClient"/> instance.
+    /// It is generally not necessary to dispose of the <see cref="IHttpClient"/> as the <see cref="IIHttpClientFactory"/> tracks and disposes resources used by the <see cref="IHttpClient"/>.
+    /// </para>
+    /// <para>
+    /// Callers are also free to mutate the returned <see cref="HttpClient"/> instance's public properties as desired.
+    /// </para>
+    /// </remarks>
+    IHttpClient CreateClient();
 }
